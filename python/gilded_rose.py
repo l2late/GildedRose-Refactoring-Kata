d9d763470228daebb6a18a9526ba55d9dfbb8926
l2late <lucadelaat@gmail.com>
2025-04-04 17:15:39 +0200

    all tests passing

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,40 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
 
+class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 0:
+                    item.quality = 0
+                elif item.sell_in < 6:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 3
+                elif item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality += 2
+                else:
+                    item.quality += 1
+                item.sell_in -= 1
+            elif item.name == "Conjured":
+                item.sell_in -= 1
+                item.quality -= 2
+            elif item.name == "Aged Brie":
+                if item.quality >= 50:
+                    item.quality = 50
+                else:
+                    item.sell_in -= 1
+                    item.quality += 1
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                item.quality = 80
+            else:
+                item.sell_in -= 1
+                item.quality -= 1
+            if item.sell_in < 0:
+                item.quality = 0
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,22 +1,44 @@
 # -*- coding: utf-8 -*-
 
-import pytest
 from gilded_rose import GildedRose, Item
 
 
-@pytest.fixture
-def aged_brie():
-    return Item("Aged Brie", 2, 0)
+def test_given_sulfuras_of_quality_80_when_update_quality_then_quality_80():
+    items = [Item("Sulfuras, Hand of Ragnaros", 1, 80)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 80
 
 
-@pytest.fixture
-def sulfuras():
-    return Item("Sulfuras, Hand of Ragnaros", 0, 80)
+def test_given_sulfuras_of_quality_80_when_update_quality_trice_then_quality_80():
+    items = [Item("Sulfuras, Hand of Ragnaros", 1, 80)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    gilded_rose.update_quality()
+    gilded_rose.update_quality()
+    assert items[0].quality == 80
+    assert items[0].sell_in == 1
 
 
-@pytest.fixture
-def backstage_pass():
-    return Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+def test_given_aged_brie_when_update_quality_then_quality_increases():
+    items = [Item("Aged Brie", 2, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 1
+
+
+def test_given_aged_brie_of_quality_49_when_update_quality_then_quality_50():
+    items = [Item("Aged Brie", 1, 49)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 50
+
+
+def test_given_aged_brie_of_quality_50_when_update_quality_then_quality_50():
+    items = [Item("Aged Brie", 1, 50)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 50
 
 
 def test_given_name_is_foo_when__is_set():
@@ -31,7 +53,6 @@ def test_given_an_item_foo_with_quality_1_when_update_quality_then_quality_is_0(
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert items[0].quality == 0
-    assert items[0].sell_in == -1
 
 
 def test_given_an_item_foo_with_quality_2_and_sellin_1_when_update_quality_then_quality_is_not_0():
@@ -39,7 +60,6 @@ def test_given_an_item_foo_with_quality_2_and_sellin_1_when_update_quality_then_
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert items[0].quality == 1
-    assert items[0].sell_in == 0
 
 
 def test_given_an_item_foo_with_quality_0_when_update_quality_then_quality_is_0():
@@ -69,3 +89,38 @@ def test_given_an_item_foo_with_quality_0_when_update_quality_twice_then_quality
     gilded_rose.update_quality()
     gilded_rose.update_quality()
     assert items[0].quality > -1
+
+
+def test_given_backstage_passes_with_quality_20_and_sellin_11_when_update_quality_then_quality_is_21():
+    items = [Item("Backstage passes to a TAFKAL80ETC concert", 11, 20)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 21
+
+
+def test_given_backstage_passes_with_quality_20_and_sellin_10_when_update_quality_then_quality_increases_by_2():
+    items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 22
+
+
+def test_given_backstage_passes_with_quality_20_and_sellin_5_when_update_quality_then_quality_increases_by_3():
+    items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 23
+
+
+def test_given_backstage_passes_with_quality_20_and_sellin_0_when_update_quality_then_quality_is_0():
+    items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 0
+
+
+def test_given_conjured_item_quality_decreases_by_2():
+    items = [Item("Conjured", 10, 20)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 18
